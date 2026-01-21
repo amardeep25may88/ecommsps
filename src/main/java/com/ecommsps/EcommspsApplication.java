43047f04c11dffc60c6b57d95c2654122831dcd0
Amardeep Singh <64483790+amardeep25may88@users.noreply.github.com>
2026-01-22 00:01:59 +0530

    Add files via upload

---
+++ src/main/java/com/ecommsps/EcommspsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommsps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommspsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommspsApplication.class, args);
+	}
+
+}
